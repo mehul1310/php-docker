b3110a469c22fb6e5f0d798984cd8ed4d6a97cf8
Mehul Joshi <mehuljoshi1310@gmail.com>
2022-09-24 21:54:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,25 +6,7 @@
 <body>
 
 <?php
-    $database ="demo";
-    $user = "root";
-    $password = "root";
-    $host = "mysql";
-
-    $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
-    $query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
-    $tables = $query->fetchAll(PDO::FETCH_COLUMN);
-
-    if (empty($tables)) {
-        echo "<p>There are no tables in database \"{$database}\".</p>";
-    } else {
-        echo "<p>Database \"{$database}\" has the following tables:</p>";
-        echo "<ul>";
-        foreach ($tables as $table) {
-            echo "<li>{$table}</li>";
-        }
-        echo "</ul>";
-    }
+    echo "Hello from docker";
 ?>
 </body>
 </html>
